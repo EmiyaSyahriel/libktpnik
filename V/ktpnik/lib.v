60be253ec9d57c4cf761e4bbabed7a6c8f183d85
Syahriel Ibnu Irfansyah <ainaaikochumpa@gmail.com>
2023-12-20 00:10:45 +0700

    vlang : added

---
+++ V/ktpnik/lib.v
@@ -0,0 +1,45 @@
+module ktpnik
+
+pub struct KtpNik {
+pub mut:
+    province u8
+    city u8
+    region u8
+    is_female bool
+    birthdate u8
+    birthmonth u8
+    birthyear u8
+    uid u16
+}
+
+pub enum KtpNikError as u32 {
+    success 		 	= 0x00000000
+    insufficient_data	= 0x86200001
+    invalid_date		= 0x86200002
+}
+
+const min_nik_info := u64(1101010101000001)
+
+pub fn ktpnik_parse(nik u64, mut retval &KtpNik) KtpNikError {
+    if nik < min_nik_info {
+        return KtpNikError.insufficient_data
+    }
+
+	retval.province   = u8((nik / 100000000000000) % 100)
+	retval.city       = u8((nik / 1000000000000) % 100)
+	retval.region     = u8((nik / 10000000000) % 100)
+	retval.birthdate  = u8((nik / 100000000) % 100)
+	retval.birthmonth = u8((nik / 1000000) % 100)
+	retval.birthyear  = u8((nik / 10000) % 100)
+	retval.uid        = u16((nik / 1) % 10000)
+	retval.is_female  = retval.birthdate > 40
+	if retval.is_female {
+		retval.birthdate -= 40
+	}
+
+    if retval.birthdate > 31 || retval.birthmonth > 12 {
+        return KtpNikError.invalid_date
+    }
+
+	return KtpNikError.success
+}
---
+++ V/sample/main.v
@@ -0,0 +1,15 @@
+module main
+import ktpnik
+
+const smp_nik_info = u64(3502136410016213)
+
+fn main() {
+	mut ktp := ktpnik.KtpNik {}
+	err := ktpnik.ktpnik_parse(smp_nik_info, mut &ktp)
+	if err != ktpnik.KtpNikError.success {
+		println("Error parsing NIK ${smp_nik_info} : ${err}");
+		exit(i32(err))
+	}
+
+	println("NIK ${smp_nik_info}, City : ${ktp.city}, Female : ${ktp.is_female}, UID : ${ktp.uid}")
+}
