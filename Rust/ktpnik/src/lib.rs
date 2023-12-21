11c5096f37683e47b3b14451b718df0b166b0315
Syahriel Ibnu Irfansyah <ainaaikochumpa@gmail.com>
2023-12-21 21:48:14 +0700

    rust : add code documentation

--- Rust/ktpnik/src/lib.rs
+++ Rust/ktpnik/src/lib.rs
@@ -1,26 +1,56 @@
+/////////////////
+/// # ktpnik - KTP NIK Parsing Library
+/// Author : EmiyaSyahriel
+/// 
+/////////////////
+
 use std::fmt;
 
-pub struct NikType {
+
+/// Data Structure of a KTP NIK
+pub struct KtpNik {
+	/// Registration Province
     pub province : u8 ,
+	/// Registration City or Regency
     pub city : u8,
+	/// Registration Region
     pub region : u8,
+	/// Is Resident a Female?
     pub is_female : bool,
+	/// Birthday Date
     pub birthdate : u8,
+	/// Birthday Month
     pub birthmonth : u8,
+	/// Birthday 2-digit Year
     pub birthyear : u8,
+	/// Person Unique ID
     pub uid : u16
 }
 
+
+/// Errors that might returned by [parse]
 #[derive(Debug, Clone, Copy)]
 pub enum KtpNikError {
+	/// No Error
     Success = 0x00000000,
+	///  Given NIK has less than required data
     InsufficientData = 0x86200001,
+	/// Given NIK is contains invalid date, 
+	/// > Note: that this only use simple check, treats that all
+	/// > months has 31 days and all years has 12 months
     InvalidDate = 0x86200002
 }
 
 const MIN_NIK_INFO : u64 = 1101010101000001;
 
-pub fn parse(nik: u64, retval: &mut NikType) -> KtpNikError {
+
+/// Parse NIK Number to [KtpNik]
+/// # Arguments
+/// * `nik` - Source KTP NIK
+/// * `retval` - Reference to a NIK Structure
+/// # Returns 
+/// Error type, [KtpNikError::Success] if success
+pub fn parse(nik: u64, retval: &mut KtpNik) -> KtpNikError {
     if nik < MIN_NIK_INFO {
         return KtpNikError::InsufficientData;
     }
@@ -30,7 +60,7 @@ pub fn parse(nik: u64, retval: &mut NikType) -> KtpNikError {
     retval.region =     ((nik / 10000000000) % 100) as u8;
     retval.birthdate =  ((nik / 100000000) % 100) as u8;
     retval.is_female = retval.birthdate > 40;
-    if(retval.is_female){
+    if retval.is_female {
         retval.birthdate -= 40;
     }
     retval.birthmonth = ((nik / 1000000) % 100) as u8;
@@ -45,6 +75,7 @@ pub fn parse(nik: u64, retval: &mut NikType) -> KtpNikError {
 }
 
 impl fmt::Display for KtpNikError {
+	/// String formatter for [KtpNikError]
     fn fmt(&self, f : &mut fmt::Formatter) -> fmt::Result {
         match self {
             KtpNikError::Success => write!(f, "Success"),
@@ -54,9 +85,10 @@ impl fmt::Display for KtpNikError {
     }
 }
 
-impl Default for NikType {
-    fn default() -> NikType{
-        NikType {
+impl Default for KtpNik {
+	/// Default Values for [KtpNik]
+    fn default() -> KtpNik{
+        KtpNik {
             province : 0,
             city : 0,
             region : 0,
--- Rust/sample/src/main.rs
+++ Rust/sample/src/main.rs
@@ -1,13 +1,19 @@
+/////////////////
+/// # ktpnik_sample - KTP NIK Parsing Library Sample
+/// Author : EmiyaSyahriel
+/// 
+/////////////////
+
 extern crate ktpnik;
 
 use std::fmt::Result;
-use ktpnik::{NikType, KtpNikError};
+use ktpnik::{KtpNik, KtpNikError};
 
 
 const SMP_NIK_INFO : u64 = 3502136410016213;
 
 fn main() -> Result {
-    let mut ktp = NikType { ..Default::default() };
+    let mut ktp = KtpNik { ..Default::default() };
     let parse_err = ktpnik::parse(SMP_NIK_INFO, &mut ktp);
     if matches!(parse_err, KtpNikError::Success)
     {
